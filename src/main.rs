a11ffcc53fb6be93d7761dc634289fbc78005b4b
Tugkan Kibar <5172699+tugkankibar@users.noreply.github.com>
2022-04-10 00:04:20 +0200

    Add health check endpoint (#3)
    * Add a working version of actix_web
    * Add health check endpoint
    * Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[get("/greet/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello_world", web::get().to(|| async { "Hello World!" }))
+            .service(health_check)
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
