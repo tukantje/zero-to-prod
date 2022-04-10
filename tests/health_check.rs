0bc8c5cb6857b2f65f5afe795cb92bf5755cab84
Tugkan Kibar <5172699+tugkankibar@users.noreply.github.com>
2022-04-10 19:53:31 +0200

    Fix tests failing when port 8080 is busy (#5)
    Add capability to get system assigned port to fix the issue.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn test_health_check() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,7 +15,11 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero_to_prod::run().expect("Failed to bind address.");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero_to_prod::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
