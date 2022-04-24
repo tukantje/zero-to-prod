7d863ab86c9a96b87936cf2d5be97cd73a92e4b5
Tugkan Kibar <tugkan@orangebandits.com>
2022-04-24 16:45:06 +0200

    Fix clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect(&format!(
-        "Failed to bind to port: {}.",
-        configuration.application_port
-    ));
+    let listener = TcpListener::bind(address).unwrap_or_else(|_| {
+        panic!(
+            "Failed to bind to port: {}.",
+            configuration.application_port
+        )
+    });
     run(listener, connection_pool)?.await
 }
