7cdf77fff3b26b471cc948a21eeec560f39c96aa
Tugkan Kibar <5172699+tugkankibar@users.noreply.github.com>
2022-04-29 16:38:57 +0200

    Add telemetry for better observability (#9)
    * Update dependencies
    
    * Add secret protection for database password
    
    * Add telemetry helpers
    
    * Add telemetry configuration to app
    
    * Add tracing to subscription endpoints
    
    * Add tracing to tests
    
    * Fix unnecessary borrow

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_to_prod::configuration::get_configuration;
+use zero_to_prod::telemetry::{get_subscriber, init_subscriber};
 
 use zero_to_prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero_to_prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).unwrap_or_else(|_| {
         panic!(
