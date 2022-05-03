1848e2b7afbb62299aabc8f7ceb653b32b0b7440
Tugkan Kibar <tugkan@orangebandits.com>
2022-05-03 15:07:47 +0200

    Increase amount of time before timeout for postgres connection

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_timeout(std::time::Duration::from_secs(30))
         .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
