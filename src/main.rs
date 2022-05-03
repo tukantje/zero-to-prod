7a97b706b3b3cc91f57899be4393a940c91211be
Tugkan Kibar <5172699+tugkankibar@users.noreply.github.com>
2022-05-03 11:59:57 +0200

    Dockerize the application & deploy to DigitalOcean (#10)
    * Add offline sqlx check
    
    * Add offline sqlx check
    
    * Dockerizes the application
    
    * Adds DigitalOcean specs for continous integration
    
    * Fix unnecessary borrow to handle clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero_to_prod::configuration::get_configuration;
 use zero_to_prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,15 +11,18 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).unwrap_or_else(|_| {
         panic!(
-            "Failed to bind to port: {}.",
-            configuration.application_port
+            "Failed to bind to: {}:{}.",
+            configuration.application.host, configuration.application.port
         )
     });
     run(listener, connection_pool)?.await
